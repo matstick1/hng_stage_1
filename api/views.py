a86c9610253a273c1d4df275849480b8ae132a21
matstick1 <mathewetchie@gmail.com>
2022-10-28 13:31:58 +0100

    create the home endpoint

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+
+class HomeView(APIView):
+    def get(self, request):
+        response = {"slackUsername": "matstick", "backend": True,
+                    "age": 25, "bio": "I love backend development"}
+        return Response(response)

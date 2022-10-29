ea93597b6c83383806ecf7c97f47f8ff998c2bec
matstick1 <mathewetchie@gmail.com>
2022-10-29 13:22:23 +0100

    setting up heroku hosting

--- stage_1/settings.py
+++ stage_1/settings.py
@@ -128,9 +128,9 @@ STATIC_ROOT =  BASE_DIR / "static"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CORS_ORIGIN_ALLOW_ALL = True
